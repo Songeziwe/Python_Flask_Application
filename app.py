cd70abeedc6e29f0dcaded99059b3fb1086e5815
Songeziwe Soboois <sobooissongeziwe@gmail.com>
2022-03-10 16:03:09 +0200

    Able to store new user to the authorized.json file.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, render_template, Response, request, url_for, redirect
-from forms import LoginForm
+from forms import LoginForm, SignupForm
 from datetime import datetime
 
 app = Flask(__name__)
@@ -15,6 +15,23 @@ def login():
         return redirect(url_for('home', message=message))
     return render_template('login.html', form=form)
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm(request.form)
+    userData = {"username":form.username.data, "password": form.password.data}
+    if form.validate_on_submit():
+        with open('./authorizedUsers.json', 'r') as f:
+            jsonFormat = f.read()                          # read from the file  
+            dataDecionary = json.loads(jsonFormat)         # convert to python dictionary
+            dataDecionary[form.username.data] = userData   # append to the dictionary
+            jsonFormat = json.dumps(dataDecionary)         # convert the dictionary back to json format
+        
+        with open('./authorizedUsers.json', 'w') as f:
+            json.dump(jsonFormat, f)
+        message = json.dumps({'username': form.username.data})
+        return redirect(url_for('home', message=message))
+    return render_template('signup.html', form=form)
+
 @app.route('/home')
 def home():
     message = json.loads(request.args['message']) # get username data from the login page
@@ -22,7 +39,7 @@ def home():
     with open('./posts.json', 'r') as f:
         posts = f.read()
     userPosts = json.loads(posts)[username]  
-    result = sorted(userPosts, key=lambda x: datetime.strptime(x['time'], '%Y-%m-%dT%H:%M:%SZ')) # sort user post in a chronollogical order
+    result = sorted(userPosts, key=lambda x: datetime.strptime(x['time'], '%Y-%m-%dT%H:%M:%SZ')) # sort user post in a chronological order
     return render_template('home.html', username = username, posts=result)
 
 @app.route('/users')
@@ -31,7 +48,9 @@ def users():
     message = json.dumps({'username': username})
     with open('./users.json', 'r') as f:
         users = f.read()
+    print(users)
     followers = json.loads(users)
+    print(followers)
     return render_template('users.html', users=followers, username=username, message=message)
 
 @app.route('/posts')
--- forms.py
+++ forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 
 class LoginForm(FlaskForm):
   username = StringField('Username')
 
+  submit = SubmitField('Login')
+
+class SignupForm(FlaskForm):
+  username = StringField('Username')
+  password = PasswordField('Password')
   submit = SubmitField('Login')
