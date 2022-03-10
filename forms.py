2150a9abdb0e2cb408cb08deb4097e0d79e7b025
Songeziwe Soboois <sobooissongeziwe@gmail.com>
2022-03-11 00:40:48 +0200

    Created authorization functionality and styling.

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, SubmitField, PasswordField
 class LoginForm(FlaskForm):
   username = StringField('Username')
   password = PasswordField('Password')
-  submit = SubmitField('Login')
+  submit = SubmitField('Sign In')
 
 class SignupForm(FlaskForm):
   username = StringField('Username')
